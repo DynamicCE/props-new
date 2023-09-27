7a73c3db8d19d7a7b7d9a11c1aa3931e868cadc5
Adınız <erkanmurat0632@gmail.com>
2023-09-27 20:24:10 +0300

    First usages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import "./App.css";
 import User from "./components/User";
 import Welcome from "./components/Welcome";
+import ToDo from "./components/ToDo";
+import UserDetails from "./User/UserDetails";
+import UserAction from "./User/UserAction";
+import UserProfile from "./User/UserProfile";
 function App() {
   return (
     <>
-      <User
-        name="erkan"
-        surname="murat"
-        isLoggedIn={true}
-        age={23}
-        section="react"
-      />
-      <User
-        name="ali"
-        surname="veli"
-        isLoggedIn={false}
-        age={25}
-        section="angular"
-      />
-      <Welcome name="erkan" />
+      <div>
+        <User
+          name="erkan"
+          surname="murat"
+          isLoggedIn={true}
+          age={23}
+          section="react"
+        />
+        <User
+          name="ali"
+          surname="veli"
+          isLoggedIn={false}
+          age={25}
+          section="angular"
+        />
+        <Welcome name="erkan" />
+        <br />
+        <ToDo todo="react çalışılacak" />
+        <br />
+        <ToDo todo="react tekrar edilecek" />
+        <br />
+        <ToDo todo="react okunacak" />
+        <br />
+        <ToDo todo="grafik çizilecek" />
+        <br />
+        <ToDo todo="Zeytin(kedim) sevilecek" />
+      </div>
+      <hr />
+      {`User Informations`}
     </>
   );
 }
---
+++ src/User/UserDetails.js
@@ -0,0 +1,3 @@
+export default function UserDetails(props) {
+  return <>{`adınız : ${props.name}${props.surname}, yaşınız: ${props.age}`}</>;
+}
